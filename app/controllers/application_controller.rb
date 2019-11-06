00ae7d605b55bbe6ff800996779c1272df75e357
webmarkyn <web.markyn@gmail.com>
2019-11-07 00:47:16 +0300

    fixed linter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,6 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
 
-
-
   def require_login
     redirect_to signin_path, notice: 'Please log in' if cookies.permanent[:token].empty?
   end
