bb85a6c3f39851c4792f0d544c7805861b6a2ff9
andynarf <untalandy@gmail.com>
2019-11-07 19:26:42 -0300

    added sign up lik, deleted migration

--- db/schema.rb
+++ db/schema.rb
@@ -20,11 +20,11 @@ ActiveRecord::Schema.define(version: 2019_11_05_203827) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
     t.string "remember_token"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
